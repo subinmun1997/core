4c56f90edf278147b0bfb6509ba3849bc3973a9c
subinmun1997 <qzxy812@gmail.com>
2022-01-19 21:49:34 +0900

    3. 스프링 컨테이너와 스프링 빈

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -46,7 +46,8 @@ public class ApplicationContextSameBeanFindTest {
             System.out.println("key = " + key + " value = " + beansOfType.get(key));
         }
         System.out.println("beansOfType = " + beansOfType);
-        assertThat(beansOfType.size()).isEqualTo(2);    }
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
 
     @Configuration
     static class SameBeanConfig {
